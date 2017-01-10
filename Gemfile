62379dd58f8aefa960b273676fa07758da641289
Leo Laksmana <beol@laksmana.com>
2017-01-10 22:01:38 +0800

    Add RPM signing

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby '~> 2.3.1'
+
+gem 'package_cloud'
+gem 'travis'
--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ set -ev
 BASE_DIR=$(dirname $0)
 
 rpmbuild -bb ${BASE_DIR}/ruby.spec
+
+find ${BASE_DIR}/rpmbuild/RPMS -type f -name "*.rpm" | \
+    xargs -I{} sh -c "${BASE_DIR}/rpm-sign.exp {} && rpm --checksig {}"
