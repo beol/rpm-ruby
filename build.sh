04eb1cdc33633d82393d910dfefca54b50628c86
Leo Laksmana <beol@laksmana.com>
2017-01-19 12:20:54 +0000

    Update build tools

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ BASE_DIR="$(dirname $0)"
 VERSION="${1:-2.3.1}"
 RELEASE="${2:-0a}"
 
-mkdir -p "${BASE_DIR}/rpmbuild/SOURCES"
-[[ -f "${BASE_DIR}/rpmbuild/SOURCES/ruby-${VERSION}.tar.gz" ]] || \
-    curl -# -L -o "${BASE_DIR}/rpmbuild/SOURCES/ruby-${VERSION}.tar.gz" "https://cache.ruby-lang.org/pub/ruby/$(echo ${VERSION} | cut -d. -f1,2)/ruby-${VERSION}.tar.gz"
+cd $BASE_DIR
 
-rpmbuild -bb --define "_version ${VERSION}" --define "_release ${RELEASE}" ${BASE_DIR}/ruby.spec
+spectool -g -R ruby.spec
 
-[[ -n "${GPG_PASSPHRASE}" ]] && find ${BASE_DIR}/rpmbuild/RPMS -type f -name "*.rpm" | xargs -I{} sh -c "${BASE_DIR}/rpm-sign.exp {} && rpm --checksig {}"
+rpmbuild -bb --define "_version ${VERSION}" --define "_release ${RELEASE}" ruby.spec
+
+[[ -n "${GPG_PASSPHRASE}" ]] && find ./rpmbuild/RPMS -type f -name "*.rpm" | xargs -I{} sh -c "./rpm-sign.exp {} && rpm --checksig {}"
